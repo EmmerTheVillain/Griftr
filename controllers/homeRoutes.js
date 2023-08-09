b1dfc5ece1460f092166cfc2f8f54038eb019788
Yajiit <tjagerter@gmail.com>
2023-08-09 19:09:36 -0400

    added homeRoutes and api/userRoutes based on activity 24 code.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,37 @@
 const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
+
+// Prevent non-logged-in users from viewing the homepage
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['first', 'ASC'], ['last', 'ASC']],
+    });
+
+    const users = userData.map((user) => user.get({ plain: true }));
+
+    res.render('homepage', {
+      users,
+      // Pass the logged-in flag to the template
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If a session exists, redirect the request to the homepage
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login', {
+    // Pass any necessary data to the login template
+  });
+});
+
+module.exports = router;
