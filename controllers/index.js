06118ef21e67b1dffb5ed19339c180b0c8a1138e
jshiffert <jshiffert@oki.org>
2023-08-10 19:28:13 -0400

    added match route and edited some of the match ui

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const matchRoutes = require('./matchRoutes')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/match', matchRoutes);
 
 module.exports = router;
---
+++ controllers/matchRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const rnum = Math.floor(Math.random()*11); // needs to be length
+        const userData = await User.findByPk(rnum, {
+            attributes: { exclude: ['password'] },
+        });
+  
+        const user = userData.get({ plain: true });
+  
+        res.render('matches', {
+            user,
+            // Pass the logged-in flag to the template
+            logged_in: req.session.logged_in,
+        });
+    // res.status(200).json(userData)
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+
+module.exports = router;
