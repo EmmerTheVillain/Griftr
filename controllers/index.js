2afb92e3820a3545d580f1bab23b9358124598db
konnor starkey <130928028+kstarkey12@users.noreply.github.com>
2023-08-16 18:33:17 -0400

    Update index.js + notes

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,26 @@
+// Import the necessary modules and middleware
 const router = require('express').Router();
 
+// Import the API routes module
 const apiRoutes = require('./api');
+
+// Import the home routes module
 const homeRoutes = require('./homeRoutes');
+
+// Import the match routes module (commented out for now)
 // const matchRoutes = require('./matchRoutes')
 
+// Use the home routes for the root path ("/")
 router.use('/', homeRoutes);
+
+// Use the API routes for paths starting with "/api"
 router.use('/api', apiRoutes);
+
+// Use the match routes for paths starting with "/match" (commented out for now)
 // router.use('/match', matchRoutes);
 
+// Serve static files from the "/images" path
 app.use("/images", express.static(path.join(__dirname, "/public/images")));
 
+// Export the router to be used in other parts of the application
 module.exports = router;
