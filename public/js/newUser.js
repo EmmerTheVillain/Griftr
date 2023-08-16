879d8ddde06c7b64625a5102c2b06972e77fd728
Yajiit <tjagerter@gmail.com>
2023-08-16 19:22:22 -0400

    added avatar, bio, and user_type to newUser signup page.

--- public/js/newUser.js
+++ public/js/newUser.js
@@ -1,26 +1,47 @@
-//currently just copy of login code
-const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to log in');
-      }
+const createUser = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-newUser').value.trim();
+  const password = document.querySelector('#password-newUser').value.trim();
+  const confirmPassword = document.querySelector('#password-confirm').value.trim();
+  const first = document.querySelector('#firstName-newUser').value.trim();
+  const last = document.querySelector('#lastName-newUser').value.trim();
+  const username = document.querySelector('#username-newUser').value.trim();
+  const bio = document.querySelector('#user-bio').value
+  const user_type = document.querySelector('#user-type').value; // User Type selection
+  const avatar = document.querySelector('#user-avatar').value; // Avatar selection
+
+  // Check if passwords match
+  if (password !== confirmPassword) {
+    alert('Passwords do not match');
+    return;
+  }
+
+  if (email && password && first && last && username) {
+    const response = await fetch('/api/users/create', {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+        first,
+        last,
+        user_type,
+        bio,
+        avatar,
+
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/'); // Redirect to homepage after creating user
+    } else {
+      alert('Failed to create user');
     }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-  
+  }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', createUser);
